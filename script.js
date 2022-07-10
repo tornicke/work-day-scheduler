c8115bfbabbd2949846874af043898b9ae07f0dc
tornicke <torniqe@gmail.com>
2022-07-10 11:18:30 +0200

    Adding all time slots, updating divs

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-// global variables
-// color code based on current time
-// show different text that user has already entered
+// Define global variables
+
+// Color code based on current time
+
+// Loop over time slots - each loop should compare current time with time slot
+// based on that add classes
+// time slot < current hour - past class to text area
+// index == current -> present
+// else - future
+
+
+// Show different text that user has already entered
+// Get the value & display
+// Loop to show the user input for each time slot
+for (let index = 9; index <= 17; index++) {
+    $(".user-input-" + index).val(localStorage.getItem(index))
+    
+}
 
 
 
-// allow user to save/update text
+// Allow user to save/update text
 
-// button - on click of that button - save user input to local storage
-// 1 event listner that will be shared by all buttons
+// Button - on click of that button - save user input to local storage
+// 1 event listener that will be shared by all buttons
 $(".submitBtn").on("click", function () {
-    // save user input to local storage
-    // search for the textarea element & grab the value
+    // Save user input to local storage
+    // Search for the textarea element & grab the value
     // $(this) is similar to current target where event was triggered
-    var text = $(this).siblings(".user-input").val();
-    // save it to localstorage corresponding to the time
+    var text = $(this).siblings(".description").val();
+    // Save it to localStorage corresponding to the time
     var time = $(this).siblings(".time-slot").attr("id");
     localStorage.setItem(time, text);
 })
